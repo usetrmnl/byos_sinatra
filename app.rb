48d01cd68c4398407f007d9f6378f33fb3a32d5a
Ryan Bianchi <1435081+qedi-r@users.noreply.github.com>
2025-02-08 10:21:13 -0500

    make the app easier to sit behind a reverse proxy

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'dotenv/load'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'debug'
+require 'uri'
 
 require_relative 'config/initializers/tailwind_form'
 require_relative 'config/initializers/explicit_forme_plugin'
@@ -10,6 +11,11 @@ require_relative 'config/initializers/explicit_forme_plugin'
 # allows access on a local network at 192.168.x.x:4567; remove to scope to localhost:4567
 set :bind, '0.0.0.0'
 set :port, 4567
+
+configure :production do
+  base_url = URI.parse(ENV['BASE_URL'])
+  use Rack::Protection::HostAuthorization, permitted_hosts: [base_url.HOST]
+end
 
 # make model, service classes accessible
 %w[models services].each do |sub_dir|
@@ -45,7 +51,7 @@ end
 # DEVICE MANAGEMENT
 def devices_form(device)
   create_forme(device, device.persisted?,
-        {autocomplete:"off", action: "/devices"},
+        {autocomplete:"off", action: "#{ENV['BASE_URL']}/devices"},
         {namespace: "device"})
 end
 
@@ -63,7 +69,7 @@ end
 get '/devices/:id/delete' do
   @device = Device.find(params[:id])
   @device.destroy
-  redirect to('/devices')
+  redirect to("#{ENV['BASE_URL']}/devices")
 end
 
 get '/devices/:id/edit' do
@@ -75,12 +81,12 @@ end
 patch '/devices/:id' do
   device = Device.find(params[:id])
   device.update(params[:device])
-  redirect to('/devices')
+  redirect to("#{ENV['BASE_URL']}/devices")
 end
 
 post '/devices' do
   Device.create!(params[:device])
-  redirect to('/devices')
+  redirect to("#{ENV['BASE_URL']}/devices")
 end
 
 get '/' do
