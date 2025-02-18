c8252724f5145e33ca455c82161de179fbdd8822
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-18 15:46:14 -0700

    Fixed Layout/RedundantLineBreak issues

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -9,18 +9,14 @@ RSpec.describe "Display path tests" do
   end
 
   it "test_it_has_a_display_path_with_a_device" do
-    dev = Device.create!({
-                           name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"
-                         })
+    dev = Device.create!({ name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01" })
     header("ACCESS_TOKEN", dev.api_key)
     _, body = get_json "/api/display/"
     expect(body["reset_firmware"]).to eq(false)
   end
 
   it "test_it_has_a_base64_display_path_with_a_device" do
-    dev = Device.create!({
-                           name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"
-                         })
+    dev = Device.create!({ name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01" })
 
     header("ACCESS_TOKEN", dev.api_key)
     _, body = get_json "/api/display/?base64=true"
