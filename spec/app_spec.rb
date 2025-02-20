b3c24d89f1340d9294f2b8ae75ea81721e7bdb15
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 14:22:52 -0700

    Fixed RuboCop RSpec/MultipleDescribes issue
    This includes improving the `get_json` method so it's more useful which required updating multiple specs. There is a bit of redundancy in these specs which needs to be addressed further in the future.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,53 +2,35 @@
 
 require_relative "spec_helper"
 
-RSpec.describe "App base path tests", type: :feature do
-  it "test_it_has_root_path" do
-    browser = get "/"
-    expect(browser.status).to be(200)
-    expect(browser.body).to match("To set up your device")
+RSpec.describe "Routes", type: :feature do
+  it "visits root and instructs how to setup device" do
+    response = get "/"
+    expect(response.body).to match("To set up your device")
   end
 
-  it "test_it_has_device_list_path" do
-    pending "Needs to be fixed"
-
-    doc = get_and_parse "/devices"
-    expect(doc.at('a[href="/devices/new"]').text.strip).to eq("Add New")
-  end
-end
-
-RSpec.describe "API path tests", type: :feature do
-  it "test_it_has_api_setup_path" do
-    _, body = get_json "/api/setup/"
-    expect(body["message"]).to eq("MAC Address not registered")
+  it "displays add new device link" do
+    response = get "/devices"
+    expect(response.body).to match(%r(<a href=".+/devices/new".+Add New.+</a>)m)
   end
 
-  it "test_it_has_api_setup_path_with_a_device" do
+  it "doesn't register device with invalid MAC address" do
+    payload = get_json "/api/setup/"
+    expect(payload[:message]).to eq("MAC Address not registered")
+  end
+
+  it "registers device" do
     mac = "aa:bb:cc:00:00:01"
-    dev = Device.create!({name: "Test Trmnl", mac_address: mac})
-    header "ID", mac
-    _, body = get_json "/api/setup/"
-    expect(body["api_key"]).to eq(dev.api_key)
-    expect(body["friendly_id"]).to eq(dev.friendly_id)
-    expect(body["image_url"]).to eq(%(#{ENV.fetch "BASE_URL"}/images/setup/setup-logo.bmp))
-    expect(body["message"]).to eq("Welcome to TRMNL BYOS")
-  end
-end
+    device = Device.create! name: "Test Trmnl", mac_address: mac
 
-RSpec.describe "Display path tests", type: :feature do
-  it "test_it_has_api_setup_path" do
-    _, body = get_json "/api/setup/"
-    expect(body["message"]).to eq("MAC Address not registered")
-  end
-
-  it "test_it_has_api_setup_path_with_a_device" do
-    mac = "aa:bb:cc:00:00:01"
-    dev = Device.create!({name: "Test Trmnl", mac_address: mac})
     header "ID", mac
-    _, body = get_json "/api/setup/"
-    expect(body["api_key"]).to eq(dev.api_key)
-    expect(body["friendly_id"]).to eq(dev.friendly_id)
-    expect(body["image_url"]).to eq(%(#{ENV.fetch "BASE_URL"}/images/setup/setup-logo.bmp))
-    expect(body["message"]).to eq("Welcome to TRMNL BYOS")
+    payload = get_json "/api/setup/"
+
+    expect(payload).to eq(
+      status: 200,
+      api_key: device.api_key,
+      friendly_id: device.friendly_id,
+      image_url: %(#{ENV.fetch "BASE_URL"}/images/setup/setup-logo.bmp),
+      message: "Welcome to TRMNL BYOS"
+    )
   end
 end
