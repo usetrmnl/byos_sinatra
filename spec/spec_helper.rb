150c807c4a1dd0ccbfb38d5208f8c1e01bec6da1
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 16:59:54 -0700

    Refactored temporary directory shared contexdt
    Necessary to clarify that this is a pure Ruby shared context (not to be confused with Sinatra) and is meant to be used by the `spec_helper.rb` only.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ SPEC_ROOT = Pathname(__dir__).realpath.freeze
 
 using Refinements::Pathname
 
-Pathname.require_tree SPEC_ROOT.join("support/shared_contexts")
+Pathname.require_tree SPEC_ROOT.join("support/shared_contexts/ruby")
 
 RSpec.configure do |config|
   config.color = true
