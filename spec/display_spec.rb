5af8acb5b9de005ddb5340abbeeb7fd9545cbed3
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 16:52:27 -0700

    Refactored specs to use shared context and subject
    Necessary to clean up the specs further by clearly defining the subject and leveraging the API shared context for improved clarity and reduced duplication.

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
-require_relative "spec_helper"
+require_relative "sinatra_helper"
 
 RSpec.describe "Display path tests", type: :feature do
+  subject(:app) { Sinatra::Application }
+
+  include_context "with API"
+
   it "test_it_has_api_display_path" do
-    payload = get_json "/api/display/"
+    get "/api/display/", format: :json
     expect(payload[:status]).to eq(404)
   end
 
   it "test_it_has_a_display_path_with_a_device" do
     device = Device.create! name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"
     header "ACCESS_TOKEN", device.api_key
-    payload = get_json "/api/display/"
+    get "/api/display/"
     expect(payload[:reset_firmware]).to be(false)
   end
 
@@ -19,7 +23,7 @@ RSpec.describe "Display path tests", type: :feature do
     pending "Needs to be fixed"
 
     header "BASE64", "true"
-    payload = get_json "/api/display/"
+    get "/api/display/"
     expect(payload[:image_url]).to include("data:image/png;base64")
   end
 
@@ -29,7 +33,7 @@ RSpec.describe "Display path tests", type: :feature do
     device = Device.create! name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"
 
     header "ACCESS_TOKEN", device.api_key
-    payload = get_json "/api/display/?base64=true"
+    get "/api/display/?base64=true"
     expect(payload[:image_url]).to include("data:image/png;base64")
   end
 end
