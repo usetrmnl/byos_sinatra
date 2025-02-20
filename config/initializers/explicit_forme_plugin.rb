201daa3f49596d68260e9dda053b751066ae3334
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 13:53:31 -0700

    Fixed Performance/RedundantBlockCall issue

--- config/initializers/explicit_forme_plugin.rb
+++ config/initializers/explicit_forme_plugin.rb
@@ -20,10 +20,10 @@ module ExplicitFormePlugin
     @to_s[form_len..]
   end
 
-  def form_output &block
+  def form_output
     form_str = String.new
     form_str << explicit_open
-    form_str << block.call if block_given?
+    form_str << yield if block_given?
     form_str << explicit_close
     form_str
   end
