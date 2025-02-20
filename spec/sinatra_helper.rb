7217a0f3153aa4eaa535b093bc8b2313bde45eb6
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 16:15:26 -0700

    Added RSpec Sinatra helper
    Necessary to have a helper for pure Ruby code (i.e. `spec_helper.rb`) and another helper for for the web framework (i.e. `sinatra_helper.rb`). This mostly pulled from link:https://alchemists.io/projects/hanamismith[Hanamismith] but adapted for Sinatra.
    
    This properly wires up Capybara so we can write spects that test the UI without having to spend so much time digging into the HTML DOM. 🎉

---
+++ spec/sinatra_helper.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+ENV["APP_ENV"] = "test"
+ENV["RACK_ENV"] = "test"
+
+require "active_record"
+require "capybara/cuprite"
+require "capybara/rspec"
+require "database_cleaner/active_record"
+require "rack/test"
+require "spec_helper"
+
+require_relative "../app"
+
+ActiveRecord::Base.establish_connection :test
+ActiveRecord::Schema.verbose = false
+load "db/schema.rb"
+
+ENV["LD_PRELOAD"] = nil
+Capybara.app = Sinatra::Application
+Capybara.server = :puma, {Silent: true, Threads: "0:1"}
+Capybara.javascript_driver = :cuprite
+Capybara.save_path = Bundler.root.join "tmp/capybara"
+Capybara.register_driver :cuprite do |app|
+  browser_options = {"disable-gpu" => nil, "disable-dev-shm-usage" => nil, "no-sandbox" => nil}
+  Capybara::Cuprite::Driver.new app, browser_options:, window_size: [1920, 1080]
+end
+
+using Refinements::Pathname
+
+Pathname.require_tree SPEC_ROOT.join("support/shared_contexts/sinatra")
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  config.include Capybara::DSL, Capybara::RSpecMatchers, :web
+  config.include Rack::Test::Methods, type: :request
+
+  config.after do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+end
