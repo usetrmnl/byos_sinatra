fba439a7c8c401e5ca40b1f9d39e60423c118d3d
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 16:15:26 -0700

    Added RSpec Sinatra helper
    Necessary to have a helper for pure Ruby code (i.e. `spec_helper.rb`) and another helper for for the web framework (i.e. `sinatra_helper.rb`). This mostly pulled from link:https://alchemists.io/projects/hanamismith[Hanamismith] but adapted for Sinatra.
    
    This properly wires up Capybara so we can write spects that test the UI without having to spend so much time digging into the HTML DOM. 🎉

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,32 +11,17 @@ unless ENV["NO_COVERAGE"]
   end
 end
 
-ENV["APP_ENV"] = "test"
-ENV["RACK_ENV"] = "test"
-
 Bundler.require :tools
 
-require "active_record"
-require "database_cleaner/active_record"
-require "nokogiri"
-require "rack/test"
 require "refinements"
 
-require_relative "../app"
-
 SPEC_ROOT = Pathname(__dir__).realpath.freeze
-
-ActiveRecord::Base.establish_connection :test
-ActiveRecord::Schema.verbose = false
-load "db/schema.rb"
 
 using Refinements::Pathname
 
 Pathname.require_tree SPEC_ROOT.join("support/shared_contexts")
 
 RSpec.configure do |config|
-  config.include Rack::Test::Methods
-
   config.color = true
   config.disable_monkey_patching!
   config.example_status_persistence_file_path = "./tmp/rspec-examples.txt"
@@ -56,28 +41,4 @@ RSpec.configure do |config|
     mocks.verify_doubled_constant_names = true
     mocks.verify_partial_doubles = true
   end
-
-  config.after do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean
-  end
-
-  def app
-    Sinatra::Application
-  end
-
-  def get_json page, query_params = {}, env = {}
-    get page, query_params, env
-    response = last_response
-    JSON response.body, symbolize_names: true
-  end
-
-  # rubocop:todo Metrics/ParameterLists
-  def post_json page, data, params = {}, env = {}
-    post page, JSON.generate(data), params, env
-    @doc = last_response
-    expect(@doc.headers["content-type"]).to eq("application/json")
-    [@doc, JSON.parse(@doc.body)]
-  end
-  # rubocop:enable Metrics/ParameterLists
 end
