bf88c6a78ea6cb006c069d2c5fd58fbc62c60202
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-20 14:22:52 -0700

    Fixed RuboCop RSpec/MultipleDescribes issue
    This includes improving the `get_json` method so it's more useful which required updating multiple specs. There is a bit of redundancy in these specs which needs to be addressed further in the future.

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -4,28 +4,32 @@ require_relative "spec_helper"
 
 RSpec.describe "Display path tests", type: :feature do
   it "test_it_has_api_display_path" do
-    _, body = get_json "/api/display/"
-    expect(body["status"]).to eq(404)
+    payload = get_json "/api/display/"
+    expect(payload[:status]).to eq(404)
   end
 
   it "test_it_has_a_display_path_with_a_device" do
-    dev = Device.create!({name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"})
-    header "ACCESS_TOKEN", dev.api_key
-    _, body = get_json "/api/display/"
-    expect(body["reset_firmware"]).to be(false)
+    device = Device.create! name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"
+    header "ACCESS_TOKEN", device.api_key
+    payload = get_json "/api/display/"
+    expect(payload[:reset_firmware]).to be(false)
   end
 
-  it "test_it_has_a_base64_display_path_with_a_device" do
+  it "answers image URL base 64 header" do
     pending "Needs to be fixed"
 
-    dev = Device.create!({name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"})
-
-    header "ACCESS_TOKEN", dev.api_key
-    _, body = get_json "/api/display/?base64=true"
-    expect(body["image_url"]).to include("data:image/png;base64")
-
     header "BASE64", "true"
-    _, body = get_json "/api/display/"
-    expect(body["image_url"]).to include("data:image/png;base64")
+    payload = get_json "/api/display/"
+    expect(payload[:image_url]).to include("data:image/png;base64")
+  end
+
+  it "answers image URL with base 64 parameter" do
+    pending "Needs to be fixed"
+
+    device = Device.create! name: "Test Trmnl", mac_address: "aa:ab:ac:00:00:01"
+
+    header "ACCESS_TOKEN", device.api_key
+    payload = get_json "/api/display/?base64=true"
+    expect(payload[:image_url]).to include("data:image/png;base64")
   end
 end
--- spec/setup_spec.rb
+++ spec/setup_spec.rb
@@ -3,19 +3,24 @@
 require_relative "spec_helper"
 
 RSpec.describe "API setup path tests", type: :feature do
-  it "test_it_has_api_setup_path" do
-    _, body = get_json "/api/setup/"
-    expect(body["message"]).to eq("MAC Address not registered")
+  it "answers not registered when MAC address is invalid" do
+    payload = get_json "/api/setup/"
+    expect(payload[:message]).to eq("MAC Address not registered")
   end
 
-  it "test_it_has_api_setup_path_with_a_device" do
+  it "registers device" do
     mac = "aa:bb:cc:00:00:01"
-    dev = Device.create!({name: "Test Trmnl", mac_address: mac})
+    device = Device.create! name: "Test Trmnl", mac_address: mac
+
     header "ID", mac
-    _, body = get_json "/api/setup/"
-    expect(body["api_key"]).to eq(dev.api_key)
-    expect(body["friendly_id"]).to eq(dev.friendly_id)
-    expect(body["image_url"]).to eq(%(#{ENV.fetch "BASE_URL"}/images/setup/setup-logo.bmp))
-    expect(body["message"]).to eq("Welcome to TRMNL BYOS")
+    payload = get_json "/api/setup/"
+
+    expect(payload).to eq(
+      status: 200,
+      api_key: device.api_key,
+      friendly_id: device.friendly_id,
+      image_url: %(#{ENV.fetch "BASE_URL"}/images/setup/setup-logo.bmp),
+      message: "Welcome to TRMNL BYOS"
+    )
   end
 end
