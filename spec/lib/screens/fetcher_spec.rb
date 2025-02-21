746f3ae691938d6b1ca39dc0e4da6e4655fc90c1
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-21 15:59:55 -0700

    Updated screen fetcher to be a instance
    Necessary to properly use a class to build an instance with the ability to inject dependencies as needed. This also adds a spec to ensure behavior is as we expect.

---
+++ spec/lib/screens/fetcher_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Screens::Fetcher do
+  using Refinements::Pathname
+
+  subject(:fetcher) { described_class.new environment: }
+
+  include_context "with temporary directory"
+
+  let(:environment) { {"BASE_URL" => "https://test.io"} }
+
+  describe "#call" do
+    let(:fixture_path) { SPEC_ROOT.join "support/fixtures/test.bmp" }
+
+    it "answers default image" do
+      expect(fetcher.call).to eq(
+        filename: "empty_state",
+        image_url: "https://test.io/images/setup/setup-logo.bmp"
+      )
+    end
+
+    it "answers generated image without encryption" do
+      path = temp_dir.join("public/images/generated").mkpath.join("test.bmp")
+      fixture_path.copy path
+
+      expect(fetcher.call(path.parent)).to eq(
+        filename: "test.bmp",
+        image_url: "https://test.io/images/generated/test.bmp"
+      )
+    end
+
+    it "answers generated image with encryption" do
+      fetcher = described_class.new(encryption: :base_64, environment:)
+
+      path = temp_dir.join("public/images/generated").mkpath.join("test.bmp")
+      fixture_path.copy path
+
+      expect(fetcher.call(path.parent)).to match(
+        filename: "test.bmp",
+        image_url: %r(data:image/bmp;base64,.+)
+      )
+    end
+
+    it "answers generated image without encryption when given invalid encryption" do
+      fetcher = described_class.new(encryption: :bogus, environment:)
+
+      path = temp_dir.join("public/images/generated").mkpath.join("test.bmp")
+      fixture_path.copy path
+
+      expect(fetcher.call(path.parent)).to eq(
+        filename: "test.bmp",
+        image_url: "https://test.io/images/generated/test.bmp"
+      )
+    end
+  end
+end
