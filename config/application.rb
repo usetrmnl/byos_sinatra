8eede968376a078216af86517d73d7b6689e4faf
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-24 15:42:45 -0700

    Added application configuration
    Necessary to setup and load the application from a single file by using the newly added Zeitwerk gem. This mimics what you'd find in a Hanami application.
    
    The `app.rb` has been refactored accordingly to show how this cleans up and simplifies the application file.

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "dotenv/load"
+require "sinatra"
+require "sinatra/activerecord"
+require "zeitwerk"
+
+Zeitwerk::Loader.new.then do |loader|
+  loader.push_dir "#{__dir__}/../lib"
+  loader.push_dir "#{__dir__}/initializers"
+  loader.tag = "trmnl-application"
+  loader.setup
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,6 +5,17 @@ require_relative "sinatra_helper"
 RSpec.describe "Routes", type: :feature do
   subject(:app) { TRMNL::Application }
 
+  describe ".loader" do
+    it "eager loads" do
+      expectation = proc { app.loader.eager_load force: true }
+      expect(&expectation).not_to raise_error
+    end
+
+    it "answers unique tag" do
+      expect(app.loader.tag).to eq("trmnl-application")
+    end
+  end
+
   it "visits root and instructs how to setup device" do
     visit "/"
     expect(page).to have_content("To set up your device")
