9f08ed2469c7195df4946444be38e8d814ae82ee
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-23 17:54:18 -0700

    Added image screensaver
    Necessary to save a web page as a screenshot. Browser settings are injected with safe defaults and `window_size` is used to resize the browser before updating the DOM and capturing the screenshot because the browser needs to be resized even though the `window_size` should be enough but isn't.
    
    The output of this object will soon be used to create an image suitable for a TRMNL device.

---
+++ lib/images/screensaver.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "ferrum"
+
+module Images
+  # Saves web page as screenshot.
+  class Screensaver
+    SETTINGS = {
+      browser_options: {
+        "disable-web-security" => nil,
+        "hide-scrollbar" => nil
+      },
+      js_errors: true,
+      window_size: [800, 480]
+    }.freeze
+
+    def initialize settings: SETTINGS, browser: Ferrum::Browser
+      @settings = settings
+      @browser = browser
+    end
+
+    def call content, path
+      save content, path
+      path
+    end
+
+    private
+
+    attr_reader :settings, :browser
+
+    def launch = browser.new settings
+
+    # :reek:FeatureEnvy
+    # :reek:TooManyStatements
+    def save content, path
+      browser.new(settings).then do |instance|
+        instance.goto "about:blank"
+        instance.resize(**dimensions)
+        instance.execute "document.documentElement.innerHTML = `#{content}`;"
+        instance.network.wait_for_idle
+        instance.screenshot path: path.to_s
+        instance.quit
+      end
+    end
+
+    def dimensions = settings.fetch(:window_size).then { |width, height| {width:, height:} }
+  end
+end
