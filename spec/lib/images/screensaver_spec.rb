b7d86c9444c1bcdc45a7d3d53df00c1f4efb53ff
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-23 17:54:18 -0700

    Added image screensaver
    Necessary to save a web page as a screenshot. Browser settings are injected with safe defaults and `window_size` is used to resize the browser before updating the DOM and capturing the screenshot because the browser needs to be resized even though the `window_size` should be enough but isn't.
    
    The output of this object will soon be used to create an image suitable for a TRMNL device.

---
+++ spec/lib/images/screensaver_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Images::Screensaver do
+  subject(:creator) { described_class.new }
+
+  include_context "with temporary directory"
+
+  describe "#call" do
+    let :content do
+      <<~CONTENT
+        <html>
+          <head>
+            <style>
+              color: black;
+              background-color: black;
+            </style>
+          </head>
+
+          <body>
+            <h1>Test</h1>
+          </body>
+        </html>
+      CONTENT
+    end
+
+    let(:path) { temp_dir.join "test.jpeg" }
+
+    it "creates screenshot" do
+      creator.call content, path
+      expect(path.exist?).to be(true)
+    end
+
+    it "answers image path" do
+      expect(creator.call(content, path)).to be(path)
+    end
+  end
+end
