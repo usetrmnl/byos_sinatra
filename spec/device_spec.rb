da5672f42ac75437cfee15b96b1cd7b6dcba7b6c
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-24 17:05:22 -0700

    Added device spec
    Necessary to ensure test coverage exists for device CRUD operations.

---
+++ spec/device_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative "sinatra_helper"
+
+RSpec.describe "Devices", type: :feature do
+  subject(:app) { TRMNL::Application }
+
+  let(:device) { Device.create! name: "Test", mac_address: "aa:bb:cc:01:02:03" }
+
+  it "displays index" do
+    device
+    visit "/devices"
+
+    expect(page).to have_content(device.friendly_id)
+  end
+
+  it "creates device" do
+    visit "/devices"
+    click_on "New"
+    fill_in "Name", with: "Test"
+    fill_in "MAC", with: "aa:bb:cc:01:02:03"
+    click_on "Save"
+
+    device = Device.find_by name: "Test"
+
+    expect(page).to have_content(device.friendly_id)
+  end
+
+  it "edits device" do
+    device
+    visit "/devices"
+    click_on "Edit"
+    fill_in "Name", with: "Test Edit"
+    click_on "Save"
+
+    expect(page).to have_content("Test Edit")
+  end
+
+  it "deletes device" do
+    device
+    visit "/devices"
+    click_on "Delete"
+
+    expect(page).to have_content("New")
+  end
+
+  it "visits home page" do
+    visit "/devices"
+    click_on "Home"
+
+    expect(page).to have_content("Welcome")
+  end
+end
