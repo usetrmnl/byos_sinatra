e2072806c34e3e858352a5f6667c49bb8450a0fd
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-25 17:16:36 -0700

    Added device part
    Necessary to present a device within the view.

---
+++ app/views/parts/device.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "hanami/view"
+
+module Views
+  module Parts
+    # The device presentation logic.
+    class Device < Hanami::View::Part
+    end
+  end
+end
