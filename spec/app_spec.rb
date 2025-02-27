06ee4b49f2776837d75d84b7cd7d9e5b4ec44f1a
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-26 20:43:13 -0700

    Added home show view
    Necessary to display the home page. The app spec was updated accordingly.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe "Routes", type: :feature do
 
   it "visits root and instructs how to setup device" do
     visit "/"
-    expect(page).to have_content("To set up your device")
+    expect(page).to have_content("Welcome")
   end
 
   it "displays add new device link" do
     visit "/devices"
-    expect(page).to have_link("Add New")
+    expect(page).to have_link("New")
   end
 end
