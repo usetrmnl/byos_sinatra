18256bf71dfffc15d2b82bd8a93e8992a0ef65a9
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-27 11:03:07 -0700

    Fixed database migration table name
    Minor cleanup to clarify the name used for the table.

--- db/migrate/20241212172428_create_devices.rb
+++ db/migrate/20241212172428_create_devices.rb
@@ -2,13 +2,13 @@
 
 class CreateDevices < ActiveRecord::Migration[8.0]
   def change
-    create_table :devices do |t|
-      t.string :name
-      t.string :mac_address
-      t.string :api_key
-      t.string :friendly_id
-      t.integer :refresh_interval, default: 900, null: false
-      t.timestamps null: false
+    create_table :devices do |table|
+      table.string :name
+      table.string :mac_address
+      table.string :api_key
+      table.string :friendly_id
+      table.integer :refresh_interval, default: 900, null: false
+      table.timestamps null: false
     end
   end
 end
