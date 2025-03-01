aa9a0c502dcd2df259162c949a227d03649146cb
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-01 11:30:21 -0700

    Fixed spec subjects
    Necessary to use clearly defined and context aware subjects.
    
    Milestone: patch

--- spec/lib/images/greyscaler_spec.rb
+++ spec/lib/images/greyscaler_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 
 RSpec.describe Images::Greyscaler do
-  subject(:creator) { described_class.new randomizer: proc { "abc123" } }
+  subject(:greyscaler) { described_class.new randomizer: proc { "abc123" } }
 
   include_context "with temporary directory"
 
@@ -12,7 +12,7 @@ RSpec.describe Images::Greyscaler do
     let(:output_path) { temp_dir.join "%<name>s.bmp" }
 
     it "creates screenshot" do
-      creator.call input_path, output_path
+      greyscaler.call input_path, output_path
       image = MiniMagick::Image.open temp_dir.join("abc123.bmp")
 
       expect(image).to have_attributes(
@@ -24,12 +24,12 @@ RSpec.describe Images::Greyscaler do
     end
 
     it "answers unique image path" do
-      expect(creator.call(input_path, output_path)).to eq(temp_dir.join("abc123.bmp"))
+      expect(greyscaler.call(input_path, output_path)).to eq(temp_dir.join("abc123.bmp"))
     end
 
     it "answers identical image path" do
       output_path = temp_dir.join "test.bmp"
-      expect(creator.call(input_path, output_path)).to eq(output_path)
+      expect(greyscaler.call(input_path, output_path)).to eq(output_path)
     end
   end
 end
